b536c38f850af531090921ea97e017fe3ebc37da
Chris-Denmark <cdenmark97@gmail.com>
2021-03-16 15:28:26 -0500

    created files for Muscle, Hacker, and LockSpecialist. Created the Muscle class

---
+++ Muscle.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Heist2
+{
+  public interface Muscle : IRobber
+  {
+    public string Name { get; set; }
+    public int SkillLevel { get; set; }
+    public int PercentageCut { get; set; }
+    public void PerformSkill(int Bank)
+    {
+      int beatenGuard = Bank - SkillLevel;
+      Console.WriteLine($"{Name} is beating up the security guards. Decreased security {SkillLevel} points");
+      if (beatenGuard <= 0)
+      {
+        Console.WriteLine($"{Name} has whooped the guards!");
+      }
+    }
+  }
+}
