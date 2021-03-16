134b660c9898042815a9e1b25703b524c5b56291
Chris-Denmark <cdenmark97@gmail.com>
2021-03-16 14:41:21 -0500

    created Bank.cs and the Bank class that represents security

---
+++ Bank.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace Heist2
+{
+  public class Bank
+  {
+    public int CashOnHand { get; set; }
+    public int AlarmScore { get; set; }
+    public int VaultScore { get; set; }
+    public int SecurityGuardScore { get; set; }
+    public bool IsSecure
+    {
+      get
+      {
+        int TotalScore = AlarmScore + VaultScore + SecurityGuardScore;
+        if (TotalScore > 0)
+        {
+          return true;
+        }
+        else
+        {
+          return false;
+        }
+      }
+    }
+  }
+}
